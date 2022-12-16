231dc9a4bc0a10a08ea1c9845122db48343d8c01
Soham Aserkar <116770046+ssaserkar@users.noreply.github.com>
2022-12-15 19:42:44 -0500

    Updated the Code for ROI Identification

--- ROI.py
+++ ROI.py
@@ -1,106 +1,49 @@
-import torch
-from torch.utils.data import Dataset
-from torchvision.io.image import ImageReadMode
-import torchvision.transforms.functional as TF
-
+import cv2
 import numpy as np
-from PIL import Image
 
-import os
-import json
-import random
+'''
+This code identifies the largest contour of the input image as we're hoping that the resultant image can be identified 
+accurately. Accordingly we are modifying bounding box of the tissue segmentation result.
 
-class SegNetDataset(Dataset):
-    '''
-    Dataset Class for Semantic Segmentation on Surgical Data
-    '''
+The box around the segmented image would look black as the image is relatively bright in nature. 
+However, as the image from the endoscope is a little darker, we have made the bounding box as 'white'
 
-    def __init__(self, root_dir, transform=None, json_path=None, sample=None, 
-                 dataset=None, image_size=[256, 256], horizontal_flip=True, brightness=True, contrast=True,
-                 rotate=True, vertical_flip=True):
-        '''
-        args:
-        root_dir (str) = File Directory with Input Surgical Images
-        json_path (str) = File with Semantic Segmentation Class information
-        sample (str) = Specify whether the sample is from train, test, or validation set
-        dataset (str) = Specify whether the Segmentation dataset is from Synapse, Cholec, or Miccai
-        '''
-        
-        # General Parameters
-        self.root_dir = root_dir
-        self.img_dir = os.path.join(root_dir, 'images')
-        self.gt_dir = os.path.join(root_dir, 'groundtruth')
-        self.image_list = [f for f in os.listdir(self.img_dir) if (f.endswith(".png") or f.endswith(".jpg"))]
-        
-        self.sample = sample
-        self.dataset = dataset
+As of now, this is an independent code, however, in the future, I am planning to embed this 
+feature as part of the neural network of UNet++ itself. 
+The combination of UNet++ and the Region of Interest work has not been done before and can be published once we run 
+for sufficient epochs and some modifications of certain hyper-parameters.
+'''
 
-        # Data Augmentation Parameters
-        self.resizedHeight = image_size[0]
-        self.resizedWidth = image_size[1]
-        self.horizontal_flip = horizontal_flip
-        self.vertical_flip = vertical_flip
-        self.rotate = rotate
-        self.brightness = brightness
-        self.contrast = contrast
+# Read in the images from the result,
+image = cv2.imread("/path/to/image1")  # path to the groundtruth or the semantically segmented image
+image_copy = cv2.imread("/path/to/image2")  # path to the original image from the endoscope
 
-        if json_path:
-            self.classes = json.load(open(json_path))["classes"]
-        
-        '''
-        Load Dataset into Memory
-        '''
+# Define the specific color you want to detect in RGB values
+rgb_color = np.array([255, 160, 165])  # colour corresponding to - Gall Bladder, since we're dealing with  
+# laparoscopic cholecystectomy. 
 
-        self.images, self.gt_images = [], []
+# convert the RGB values to Hue, Saturation and Vibrance Colour Space
+hsv_color = cv2.cvtColor(rgb_color, cv2.COLOR_RGB2HSV)
 
-        for idx, val in enumerate(self.image_list):
-            img_name = os.path.join(self.img_dir, self.image_list[idx])
+# Define a range of colors around the specific color in the HSV color space
+color_lower = np.array([hsv_color[0] - 10, 100, 100])
+color_upper = np.array([hsv_color[0] + 10, 255, 255])
 
-            if self.dataset == "synapse":
-                gt_file_name = self.image_list[idx][0:-4] + ".png"
-            elif self.dataset == "cholec":
-                gt_file_name = self.image_list[idx][0:-4] + "_color_mask.png"
-            elif self.dataset == "miccai":
-                gt_file_name = self.image_list[idx][0:-4] + "_gt.png"
-            else:
-                raise ValueError("Ground Truth File Name Does Not Exist")
+# Create a mask for the image based on the defined color range
+mask = cv2.inRange(image, color_lower, color_upper)
 
-            gt_name = os.path.join(self.gt_dir, gt_file_name)
+# Find contours in the mask
+contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            image = Image.open(img_name)
-            image = image.convert("RGB")
+# Iterate through the contours and draw a bounding box around the largest one
+if contours:
+    c = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 0), 2)  # Black bounding box
+    cv2.rectangle(image_copy, (x, y), (x + w, y + h), (255, 255, 255), 2)  # White bounding box
 
-            gt = Image.open(gt_name)
-            gt = gt.convert("RGB")
-
-            self.images.append(image)
-            print(f"loaded image: {img_name}")
-            self.gt_images.append(gt)
-            print(f"loaded gt: {gt_name}")
-
-    def __len__(self):
-        return len(self.image_list)
-
-    def __getitem__(self, idx):
-        #img_name = os.path.join(self.img_dir, self.image_list[idx])
-
-        #if self.dataset == "synapse":
-        #    gt_file_name = self.image_list[idx][0:-4] + ".png"
-        #elif self.dataset == "cholec":
-        #    gt_file_name = self.image_list[idx][0:-4] + "_color_mask.png"
-        #elif self.dataset == "miccai":
-        #    gt_file_name = self.image_list[idx][0:-4] + "_gt.png"
-        #else:
-        #    raise ValueError("Ground Truth File Name Does Not Exist")
-
-        #gt_name = os.path.join(self.gt_dir, gt_file_name)
-
-        #image = Image.open(img_name)
-        #image = image.convert("RGB")
-
-        #gt = Image.open(gt_name)
-        #gt = gt.convert("RGB")
-
-        image, gt = self.images[idx], self.gt_images[idx]
-        
-        
+# Show the images
+cv2.imshow("Image 1", image)
+cv2.imshow("Image 2", image_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
